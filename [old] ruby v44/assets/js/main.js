d78c95fc5ec2229901bf67df21e9571d0532c6f7
marleymcbride <business.marleymcbride@gmail.com>
2025-07-15 15:28:50 +0100

    Working site v2 - 15/7/25

---
+++ [old] ruby v44/assets/js/main.js
@@ -0,0 +1,9 @@
+(function () {
+    // Simple pagination function - no-op if not needed
+    function pagination(enable) {
+        // Placeholder for pagination functionality
+        return enable;
+    }
+
+    pagination(true);
+})();
